857cf538855a702ee18f2cfcd4489ac7e82b612b
UladzimirKulakou <rombik9@ya.ru>
2021-07-15 13:34:12 +0300

    commit 1
     Create project about LifeCycles on main

---
+++ HomeWork_18_LifeCycles/HomeWork_18_LifeCycles/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWork_18_LifeCycles
+//
+//  Created by Владимир  on 7/15/21.
+//  Copyright © 2021 Владимир . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
